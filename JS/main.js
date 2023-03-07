9b88e44228a6e4fcf2b643ef932c3feed84d5fd9
HectorKim <yakima1019@gmail.com>
2023-03-07 16:31:13 +0900

    JS: Arrow up

--- JS/main.js
+++ JS/main.js
@@ -38,9 +38,20 @@ document.addEventListener('scroll', ()=>{
 });
 
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow_up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight) {
+    arrowUp.classList.add('visible');
+  }else {
+    arrowUp.classList.remove('visible');
+  }
+});
 
-
-
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+scrollIntoView('#home');
+});
 
 
 
